d8ceab0cd90d09e0890451197a5015e3a5f05ac9
Dmitry Kazimirov <dk@revl.org>
2011-07-25 18:24:30 -0400

    First working version of atom_to_todotxt.py.

--- atom_to_todotxt.py
+++ atom_to_todotxt.py
@@ -3,20 +3,17 @@
 import xml.dom.minidom
 import codecs
 import sys
-
-global output_file
+import re
 
 class Task:
     def __init__(self):
-        self.text = ''
-        self.date = None
-        self.url = None
-        self.projects = None
-        self.context = None
-
-        self.updated = None
-        self.title = ''
-        self.meta = None
+        self.__priority = ''
+        self.__date = ''
+        self.__descr = ''
+        self.__url = ''
+        self.__notes = ''
+        self.__project = ''
+        self.__contexts = set()
 
     def parse_entry(self, entry):
         for field in entry.childNodes:
@@ -24,26 +21,26 @@ class Task:
                 # Ignore these elements
                 pass
             elif field.localName == 'updated':
-                task.updated = field.firstChild.nodeValue
-                time_pos = task.updated.find('T')
+                self.__date = field.firstChild.nodeValue
+                time_pos = self.__date.find('T')
                 if time_pos >= 0:
-                    task.updated = task.updated[:time_pos]
+                    self.__date = self.__date[:time_pos]
+                self.__date += ' '
             elif field.localName == 'title':
-                task.title = field.firstChild.nodeValue
+                self.__descr = field.firstChild.nodeValue
             elif field.localName == 'content':
-                task.meta = self.process_content(field)
+                self.__process_content(field)
             else:
                 raise Exception, 'Unknown node ' + field.localName
 
-    def output(self, output_file):
-        if self.updated:
-            print >> output_file, self.updated,
-        if self.meta:
-            print >> output_file, self.title, self.meta
-        else:
-            print >> output_file, self.title
+    def convert(self):
+        result = self.__priority + self.__date + self.__descr + \
+            self.__url + self.__notes + self.__project
+        if self.__contexts:
+            result += ' @' + ' @'.join(self.__contexts)
+        return result
 
-    def process_subdiv(self, subdiv):
+    def __process_subdiv(self, subdiv):
         subdiv_class = subdiv.attributes['class'].nodeValue
         if subdiv_class.startswith('rtm_'):
             subdiv_class = subdiv_class[4:]
@@ -51,7 +48,16 @@ class Task:
             # Ignore these fields
             pass
         elif subdiv_class == 'notes':
-            print >> output_file, subdiv_class
+            for note_div in subdiv.childNodes:
+                note_class = note_div.attributes['class'].nodeValue
+                if note_class != 'rtm_note':
+                    raise Exception, 'Invalid note class: ' + note_class
+                if note_div.childNodes.length != 3:
+                    raise Exception, 'Unexpected number of children in a note'
+                (note_title, note_content) = note_div.childNodes[:2]
+                self.__notes += ' ' + \
+                    note_title.firstChild.firstChild.nodeValue + ': ' + \
+                    repr(note_content.firstChild.nodeValue)
         else:
             if subdiv.childNodes.length != 2:
                 raise Exception, 'Invalid subdiv node size (' + \
@@ -62,45 +68,56 @@ class Task:
                 value = value.firstChild
             value = value.nodeValue
 
-            #if value != 'none':
-            if subdiv_class == 'url':
-                print >> output_file, subdiv_class + ': ' + value
-            elif subdiv_class == 'list':
-                print >> output_file, subdiv_class + ': ' + value
-            else:
-                print >> output_file, subdiv_class + ': ' + value
+            if value != 'none':
+                if subdiv_class == 'priority':
+                    self.__priority = '(' + chr(ord(value) + 16) + ') '
+                elif subdiv_class == 'url':
+                    self.__url = ' URL: ' + value
+                elif subdiv_class == 'list':
+                    self.__project = ' +' + self.__capitalize(value)
+                elif subdiv_class == 'location':
+                    self.__contexts.add(self.__capitalize(value))
+                elif subdiv_class == 'tags':
+                    for tag in value.split(', '):
+                        self.__contexts.add(self.__capitalize(tag))
+                else:
+                    raise Exception, 'Invalid subdiv class: ' + subdiv_class
 
-    def process_content(self, content):
+    def __process_content(self, content):
         if content.childNodes.length != 1:
             raise Exception, 'Invalid content length'
 
-        div = content.firstChild
-        if div.localName != 'div':
-            raise Exception, 'Unknown content node ' + div.localName
+        content = content.firstChild
+        if content.localName != 'div':
+            raise Exception, 'Unexpected content node ' + content.localName
 
-        meta = ''
-
-        for subdiv in div.childNodes:
+        for subdiv in content.childNodes:
             if subdiv.localName != 'div':
                 raise Exception, 'Unknown div node ' + subdiv.localName
+            self.__process_subdiv(subdiv)
 
-            field = self.process_subdiv(subdiv)
-            if field:
-                if meta:
-                    meta = meta + ' ' + field
-                else:
-                    meta = field
-
-        return meta
+    @staticmethod
+    def __capitalize(words):
+        result = ''
+        for word in re.split('(?:\W|_)+', words):
+            if word:
+                if word[:1].islower():
+                    word = word.capitalize()
+                result += word
+        return result
 
 try:
     dom = xml.dom.minidom.parse('Atom_Feed.xml')
-    output_file = codecs.open('todo.txt.RTM', 'w', 'utf-8')
+    output_file_name = 'todo.txt.RTM'
+    output_file = codecs.open(output_file_name, 'w', 'utf-8')
 
     for entry in dom.getElementsByTagName('entry'):
-        print >> output_file, '--------------------------------'
         task = Task()
         task.parse_entry(entry)
-        task.output(output_file)
+        print >> output_file, task.convert()
+
+    print 'Conversion completed. Please carefully review the contents of'
+    print output_file_name + ' before merging it into your todo.txt.'
+
 except Exception, e:
     print >> sys.stderr, e
