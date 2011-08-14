7106c5f9e96d1d6bc04f6092d1a442e69aaf852b
Dmitry Kazimirov <dk@revl.org>
2011-08-13 21:35:08 -0400

    First working version of ics_to_todotxt.py.

--- ics_to_todotxt.py
+++ ics_to_todotxt.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import codecs
-import sys
 import re
 
 class ICSParser:
@@ -41,52 +40,71 @@ class ICSParser:
             if not key in self.node:
                 self.node[key] = val
 
-class Task:
-    def __init__(self):
-        self.__priority = ''
-        self.__dates = set()
-        self.__descr = ''
-        self.__url = ''
-        self.__notes = ''
-        self.__project = ''
-        self.__contexts = set()
+def convert_date(date):
+    time_pos = date.find('T')
+    if time_pos >= 0:
+        date = date[:time_pos]
+    if len(date) == 8:
+        date = date[:4] + '-' + date[4:6] + '-' + date[6:]
+    return date
 
-    def parse_entry(self, entry):
-        for field in entry.childNodes:
-            if field.localName in ('author', 'link', 'id'):
-                # Ignore these elements
-                pass
-            elif field.localName == 'updated':
-                self.__date = field.firstChild.nodeValue
-                time_pos = self.__date.find('T')
-                if time_pos >= 0:
-                    self.__date = self.__date[:time_pos]
-                self.__date += ' '
-            elif field.localName == 'title':
-                self.__descr = field.firstChild.nodeValue
-            elif field.localName == 'content':
-                self.__process_content(field)
-            else:
-                raise Exception, 'Unknown node ' + field.localName
+def capitalize(words):
+    result = ''
+    for word in re.split('(?:\W|_)+', words):
+        if word:
+            if word[:1].islower():
+                word = word.capitalize()
+            result += word
+    return result
 
-    def convert(self):
-        result = self.__priority + self.__date + self.__descr + \
-            self.__url + self.__notes + self.__project
-        if self.__contexts:
-            result += ' @' + ' @'.join(self.__contexts)
-        return result
+def unescape(string):
+    return re.sub(r'\\(.)', r'\1', string)
 
-try:
-    input_file = codecs.open('iCalendar_Service.ics', 'r', 'utf-8')
-    tasks = ICSParser().parse_file(input_file)['VCALENDAR'][0]['VTODO']
-    output_file_name = 'todo.txt.RTM'
-    output_file = codecs.open(output_file_name, 'w', 'utf-8')
+input_file = codecs.open('iCalendar_Service.ics', 'r', 'utf-8')
+todos = ICSParser().parse_file(input_file)['VCALENDAR'][0]['VTODO']
+output_file_name = 'todo.txt.RTM'
+output_file = codecs.open(output_file_name, 'w', 'utf-8')
 
-    for task in tasks:
-        print >> output_file, repr(task)
+for todo in todos:
+    output_line = ''
+    if 'STATUS' in todo and todo['STATUS'] == 'COMPLETED':
+        output_line = 'x '
+    if 'PRIORITY' in todo:
+        output_line += '(' + \
+            chr(((ord(todo['PRIORITY']) - 49) >> 2) + 65) + ') '
+    dates = set()
+    for field in 'DTSTAMP', 'DTSTART', 'LAST-MODIFIED', 'COMPLETED':
+        if field in todo:
+            dates.add(convert_date(todo[field]))
+    for date in sorted(dates, reverse=True):
+        output_line += date + ' '
+    output_line += unescape(todo['SUMMARY'])
+    if 'URL' in todo:
+        output_line += ' ' + todo['URL']
+    if 'DUE' in todo:
+        output_line += ' due:' + convert_date(todo['DUE'])
+    sep = ' '
+    next_sep = '; '
+    for field in todo['DESCRIPTION'].split(r'\n'):
+        if field.startswith('Time estimate:') or \
+                field.startswith('Updated:') or not field:
+            continue
+        elif field.startswith('Tags:'):
+            tags = field[5:].strip()
+            if tags != 'none':
+                for tag in unescape(tags).split(','):
+                    output_line += ' @' + capitalize(tag)
+        elif field.startswith('Location:'):
+            location = field[9:].strip()
+            if location != 'none':
+                output_line += ' @' + capitalize(location)
+        elif re.match('^--+$', field):
+            next_sep = ': '
+        else:
+            output_line += sep + unescape(field)
+            sep = next_sep
+            next_sep = '; '
+    print >> output_file, output_line
 
-    print 'Conversion completed. Please carefully review the contents of'
-    print output_file_name + ' before merging it into your todo.txt.'
-
-except Exception, e:
-    print >> sys.stderr, e
+print 'Conversion completed. Please carefully review the contents of'
+print output_file_name + ' before merging it into your todo.txt.'
